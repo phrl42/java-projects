7002b149c5559fdc2bc032f37bed292bc83e90f7
pherel <phrl42@riseup.net>
2025-04-25 13:52:16 +0200

    working on selection sort and sorting abstraction

---
+++ Data Structures and Algorithms/sorting/src/main/java/Main/SelectionSort.java
@@ -0,0 +1,12 @@
+package Main;
+
+import java.util.List;
+import java.util.LinkedList;
+
+public class SelectionSort implements Sorting {
+  
+  @Override
+  public List<Integer> sort(final List<Integer> list){
+    return new LinkedList<Integer>();
+  }
+}
--- Data Structures and Algorithms/sorting/src/main/java/Main/Sorting.java
+++ Data Structures and Algorithms/sorting/src/main/java/Main/Sorting.java
@@ -1,5 +1,8 @@
 package Main;
 
+import java.util.List;
+
 public interface Sorting {
   
+  List<Integer> sort(final List<Integer> list);
 }
